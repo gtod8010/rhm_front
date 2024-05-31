c45034d9f235c9841984d68250700faebd25f659
DONGHYUN KANG <gtod8010@naver.com>
2024-05-31 10:16:37 +0900

    'feat: 프록시 설정'

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@ import dayjs from 'dayjs';
 
 export const setLogin = async (username, password) => {
   try {
-    const response = await axios.post('http://rhm.co.kr/login', { username, password });
+    const response = await axios.post('/api/login', { username, password });
     localStorage.setItem('token', response.data.token);
     localStorage.setItem('user', JSON.stringify(response.data.userInfo)); // 사용자 정보를 로컬 스토리지에 저장
     return response.data.userInfo; // 사용자 정보를 반환
@@ -21,29 +21,29 @@ export const setLogin = async (username, password) => {
 
 
 export const fetchMembers = async (username) => {
-  const response = await axios.post('http://rhm.co.kr/members', { username });
+  const response = await axios.post('/api/members', { username });
   return response.data.map((row, index) => ({ ...row, idx: index + 1, created_at : new Date(row.created_at) }));
 
 };
 
 export const addMember = async (newRow) => {
-  const response = await axios.post('http://rhm.co.kr/addMember', newRow);
+  const response = await axios.post('/api/addMember', newRow);
   return response.data;
 };
 
 export const deleteMembers = async (idsToDelete) => {
-  await axios.post('http://rhm.co.kr/deleteMembers', { ids: idsToDelete });
+  await axios.post('/api/deleteMembers', { ids: idsToDelete });
 };
 
 export const editMember = async (editMemberData) => {
-  const response = await axios.post('http://rhm.co.kr/editMember', editMemberData);
+  const response = await axios.post('/api/editMember', editMemberData);
   return response.data;
 };
 
 // -------------------------------------- reward -------------------------------------- //
 
 export const getRewards = async (username) => {
-  const response = await axios.post('http://rhm.co.kr/getRewards', { username });
+  const response = await axios.post('/api/getRewards', { username });
   return response.data.map(item => ({
     ...item,
     start_date: new Date(item.start_date),
@@ -52,7 +52,7 @@ export const getRewards = async (username) => {
 };
 
 export const addReward = async (rewardData) => {
-  const response = await axios.post('http://rhm.co.kr/addReward', rewardData);
+  const response = await axios.post('/api/addReward', rewardData);
   return {
     ...response.data,
     start_date: new Date(response.data.start_date),
@@ -61,7 +61,7 @@ export const addReward = async (rewardData) => {
 };
 
 export const editReward = async (rewardData) => {
-  const response = await axios.post('http://rhm.co.kr/editReward', rewardData);
+  const response = await axios.post('/api/editReward', rewardData);
   return{
     ...response.data,
     start_date: new Date(response.data.start_date),
@@ -71,7 +71,7 @@ export const editReward = async (rewardData) => {
 };
 
 export const extendReward = async (idx, end_date) => {
-  const response = await axios.post('http://rhm.co.kr/extendReward',{idx,end_date})
+  const response = await axios.post('/api/extendReward',{idx,end_date})
       return {
       ...response.data,
       start_date: new Date(response.data.start_date),
@@ -83,7 +83,7 @@ export const extendReward = async (idx, end_date) => {
 export const extendMultipleRewards = async (selectionModel, endDate) => {
   const updatedRows = await Promise.all(
     selectionModel.map(async (idx) => {
-      const response = await axios.post('http://rhm.co.kr/extendReward', {
+      const response = await axios.post('/api/extendReward', {
         idx: idx,
         end_date: endDate,
       });
@@ -98,13 +98,13 @@ export const extendMultipleRewards = async (selectionModel, endDate) => {
 };
 
 export const deleteReward = async (ids) => {
-  await axios.post('http://rhm.co.kr/deleteRewards', { ids });
+  await axios.post('/api/deleteRewards', { ids });
 };
 
 // -------------------------------------- status --------------------------------------
 
 export const updateUsedStatus = async (idx, newStatus) => {
-  const response = await axios.post('http://rhm.co.kr/updateUsedStatus', { idx, used: newStatus });
+  const response = await axios.post('/api/updateUsedStatus', { idx, used: newStatus });
   return{
     ...response.data,
     start_date: new Date(response.data.start_date),
